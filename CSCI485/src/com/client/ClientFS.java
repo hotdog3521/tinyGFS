7ba6e2815ffe6f325640181073e41a0095d5a81d
Taegyum Kim <taegyumk@usc.edu>
2016-01-17 10:42:49 -0800

    Start from scratch

--- CSCI485/src/com/client/ClientFS.java
+++ CSCI485/src/com/client/ClientFS.java
@@ -1,53 +1,7 @@
 package com.client;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import com.chunkserver.ChunkServer;
-
 public class ClientFS {
-	// 3000 for now 
-	static int ServerPort = 3000;
-	static Socket ClientSocket;
-	static ObjectOutputStream WriteOutput;
-	static ObjectInputStream ReadInput;
-	
-	public ClientFS(){
-		if (ClientSocket != null) return;
-		//The client is already connected
-//			BufferedReader binput = new BufferedReader(new FileReader(ChunkServer.ClientConfigFile));
-//			String port = binput.readLine();
-//			port = port.substring( port.indexOf(':')+1 );
-//			ServerPort = Integer.parseInt(port);
-			try{
-				ClientSocket = new Socket("localhost", 3000);
-				
-			} catch(IOException ioe) {
-				ioe.printStackTrace();
-			}
-			
-			
-//			WriteOutput = new ObjectOutputStream(ClientSocket.getOutputStream());
-//			ReadInput = new ObjectInputStream(ClientSocket.getInputStream());
-		}
-	
-	
+
 	public enum FSReturnVals {
 		DirExists, // Returned by CreateDir when directory exists
 		DirNotEmpty, //Returned when a non-empty directory is deleted
@@ -69,173 +23,10 @@ public class ClientFS {
 	 * SrcDirNotExistent if the src directory does not exist Returns
 	 * DestDirExists if the specified dirname exists
 	 *
-	 * Example usage: CreateDir("/", "Shahram"), CreateDir("/Shahram",
-	 * "CSCI485"), CreateDir("/Shahram/CSCI485", "Lecture1")
-	 * @throws IOException 
+	 * Example usage: CreateDir("/", "Shahram"), CreateDir("/Shahram/",
+	 * "CSCI485"), CreateDir("/Shahram/CSCI485/", "Lecture1")
 	 */
-	public FSReturnVals CreateDir(String src, String dirname){
-		
-		
-		
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try {
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		// add dashes to separate the message
-		String createDirCMD = "createDir-";
-		src += "-";
-		
-		byte[] CMD = createDirCMD.getBytes();
-		byte[] srcBytes = src.getBytes();
-		byte[] dirnameBytes = dirname.getBytes();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try {
-			outputStream.write(CMD);
-			outputStream.write(srcBytes);
-			outputStream.write(dirnameBytes);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		
-		try{
-			
-			
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.SrcDirNotExistent;
-					case 1:
-						return FSReturnVals.DestDirExists;
-					case 2:
-						return FSReturnVals.Success;
-					
-					}
-				}
-			}
-			
-		}catch(Exception e){
-			
-		}
-	
-		return null;
-	}
-	
-public String[] ListDir(String tgt) {
-		
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try {
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		String listDirCMD = "listDir-";
-		tgt += "-";
-		byte[] CMD = listDirCMD.getBytes();
-		byte[] targetSrc = tgt.getBytes();
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try {
-			outputStream.write(CMD);
-			outputStream.write(targetSrc);
-			
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		byte [] message = outputStream.toByteArray();
-		
-		try{
-			dos.write(message);
-			dos.flush();
-			
-			boolean accepted = false;
-			byte [] fullList = null;
-
-			while(!accepted)
-			{
-				int count = 0;
-				
-				outputStream = new ByteArrayOutputStream( );
-				byte [] standardMessage = null;
-				while(dis.available() > 0)
-				{
-					if(count > 10)
-					{
-						break;
-					}
-					
-					standardMessage = new byte[dis.available()];
-					dis.read(standardMessage);
-					outputStream.write(standardMessage);
-					count++;
-					accepted = true;
-				}
-
-			}
-				fullList = outputStream.toByteArray();
-				String str = new String(fullList, Charset.forName("UTF-8"));
-				
-				int result;
-				String[] returnArray = str.split("-");
-				
-				if(returnArray[0].equals("DoesNotExist"))
-				{
-					result = 0;
-				}
-				
-				else
-				{
-					result = 1;
-				}
-				
-				switch(result){
-				case 0: // directory does not exist
-					
-				
-					returnArray = new String[1];
-					returnArray[0] = FSReturnVals.SrcDirNotExistent.toString();
-					return returnArray;
-				case 1: // directory exists
-					System.out.println("returns Array");
-					return returnArray;
-				case 2:
-					return null;
-				}
-			}
-		
-		catch(Exception e){
-			
-		}
-		
+	public FSReturnVals CreateDir(String src, String dirname) {
 		return null;
 	}
 
@@ -244,71 +35,9 @@ public String[] ListDir(String tgt) {
 	 * SrcDirNotExistent if the src directory does not exist Returns
 	 * DestDirExists if the specified dirname exists
 	 *
-	 * Example usage: DeleteDir("/Shahram/CSCI485", "Lecture1")
-	 * @throws IOException 
+	 * Example usage: DeleteDir("/Shahram/CSCI485/", "Lecture1")
 	 */
 	public FSReturnVals DeleteDir(String src, String dirname) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-	
-		
-		String deleteDirCMD = "deleteDir-";
-
-		src += "-";
-		
-		byte[] CMD = deleteDirCMD.getBytes();
-		byte[] srcBytes = src.getBytes();
-		byte[] dirnameBytes = dirname.getBytes();
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(srcBytes);
-			outputStream.write(dirnameBytes);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.SrcDirNotExistent;
-					case 1:
-						return FSReturnVals.DirNotEmpty;
-					case 2:
-						return FSReturnVals.Success;
-					
-					}
-				}
-			}		
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-			
 		return null;
 	}
 
@@ -317,71 +46,10 @@ public String[] ListDir(String tgt) {
 	 * Returns SrcDirNotExistent if the src directory does not exist Returns
 	 * DestDirExists if a directory with NewName exists in the specified path
 	 *
-	 * Example usage: RenameDir("/Shahram/CSCI485", "CSCI550") changes
+	 * Example usage: RenameDir("/Shahram/CSCI485", "/Shahram/CSCI550") changes
 	 * "/Shahram/CSCI485" to "/Shahram/CSCI550"
 	 */
 	public FSReturnVals RenameDir(String src, String NewName) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-
-		String renameDirCMD = "renameDir-";
-
-		src += "-";
-	
-		byte[] CMD = renameDirCMD.getBytes();
-		byte[] srcBytes = src.getBytes();
-		byte[] newNameBytes = NewName.getBytes();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(srcBytes);
-			outputStream.write(newNameBytes);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.SrcDirNotExistent;
-					case 1:
-						return FSReturnVals.DirNotEmpty;
-					case 2:
-						return FSReturnVals.Success;
-					
-					}
-				}
-			}		
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-		
 		return null;
 	}
 
@@ -390,84 +58,21 @@ public String[] ListDir(String tgt) {
 	 * the target directory does not exist Returns null if the target directory
 	 * is empty
 	 *
-	 *xample usage: ListDir("/Shahram/CSCI485")
+	 * Example usage: ListDir("/Shahram/CSCI485")
 	 */
-	
+	public String[] ListDir(String tgt) {
+		return null;
+	}
 
 	/**
 	 * Creates the specified filename in the target directory Returns
 	 * SrcDirNotExistent if the target directory does not exist Returns
 	 * FileExists if the specified filename exists in the specified directory
 	 *
-	 * Example usage: Createfile("/Shahram/CSCI485/Lecture1", "Intro.pptx")
+	 * Example usage: Createfile("/Shahram/CSCI485/Lecture1/", "Intro.pptx")
 	 */
 	public FSReturnVals CreateFile(String tgtdir, String filename) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-
-		
-
-		tgtdir += "-";
-	
-		String createFileCMD = "createFile-";
-		byte[] CMD = createFileCMD.getBytes();
-		byte[] targetDirBytes = tgtdir.getBytes();
-		byte[] fileNameBytes = filename.getBytes();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(targetDirBytes);
-			outputStream.write(fileNameBytes);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.SrcDirNotExistent;
-					case 1:
-						return FSReturnVals.FileExists;
-					case 2:
-						return FSReturnVals.Success;
-													
-					}
-				}
-			}		
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-		
 		return null;
-		
-		
-		
 	}
 
 	/**
@@ -475,79 +80,10 @@ public String[] ListDir(String tgt) {
 	 * if the target directory does not exist Returns FileDoesNotExist if the
 	 * specified filename is not-existent
 	 *
-	 * Example usage: DeleteFile("/Shahram/CSCI485/Lecture1", "Intro.pptx")
+	 * Example usage: DeleteFile("/Shahram/CSCI485/Lecture1/", "Intro.pptx")
 	 */
 	public FSReturnVals DeleteFile(String tgtdir, String filename) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-
-		
-
-		tgtdir += "-";
-	
-		String deleteFileCMD = "deleteFile-";
-		byte[] CMD = deleteFileCMD.getBytes();
-		byte[] targetDirBytes = tgtdir.getBytes();
-		byte[] fileNameBytes = filename.getBytes();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(targetDirBytes);
-			outputStream.write(fileNameBytes);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.SrcDirNotExistent;
-					case 1:
-						return FSReturnVals.FileDoesNotExist;
-					case 2:
-						return FSReturnVals.Success;
-													
-					}
-				}
-			}		
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-		
 		return null;
-		
-		
-		
-		
-		
-		
-		
 	}
 
 	/**
@@ -555,116 +91,10 @@ public String[] ListDir(String tgt) {
 	 * Returns FileDoesNotExist if the specified filename by FilePath is
 	 * not-existent
 	 *
-	 * Example usage: OpenFile("/Shahram/CSCI485/Lecture1/Intro.pptx")
+	 * Example usage: OpenFile("/Shahram/CSCI485/Lecture1/Intro.pptx", FH1)
 	 */
 	public FSReturnVals OpenFile(String FilePath, FileHandle ofh) {
-
-		
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-
-		
-
-		FilePath += "-";
-	
-		String deleteFileCMD = "openFile-";
-		byte[] CMD = deleteFileCMD.getBytes();
-		byte[] filePath = FilePath.getBytes();
-
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(filePath);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			boolean accepted = false;
-			byte [] fullList = null;
-			while(!accepted)
-			{
-				int count = 0;
-				
-				outputStream = new ByteArrayOutputStream( );
-				byte [] standardMessage = null;
-				while(dis.available() > 0)
-				{
-					if(count > 10)
-					{
-						break;
-					}
-					
-					standardMessage = new byte[dis.available()];
-					dis.read(standardMessage);
-					outputStream.write(standardMessage);
-					count++;
-					accepted = true;
-				}
-
-			}
-				fullList = outputStream.toByteArray();
-				String str = new String(fullList, Charset.forName("UTF-8"));
-				
-				int result;
-				String[] returnArray = str.split("-");
-				
-				if(returnArray[0].equals("fileNotExist"))
-				{
-					result = 0;
-				}
-				
-				else
-				{
-					result = 1;
-					//success
-					ofh.setFilePath(FilePath);
-					if(returnArray[0].equals("emptyFile")){
-						ArrayList<String> handlesInArrayList = new ArrayList<String>();
-						ofh.setChunkHandles(handlesInArrayList);
-					}else{
-						int sizeOfChunkLists = Integer.parseInt(returnArray[0]);
-						ArrayList<String> handlesInArrayList = new ArrayList<String>();
-			
-						for(int i = 0 ; i < sizeOfChunkLists; i ++){
-							handlesInArrayList.add(returnArray[1 + i]);
-							ofh.setLastOffset(returnArray[1 + i], Integer.parseInt(returnArray[1 + i + sizeOfChunkLists]));
-						}
-
-						ofh.setChunkHandles(handlesInArrayList);
-						
-					}
-				}
-				
-				switch(result){
-				case 0:
-					return FSReturnVals.SrcDirNotExistent;
-				case 1:
-					return FSReturnVals.Success;
-				}
-		
-	
-		
-	
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-		
 		return null;
-		
-	
 	}
 
 	/**
@@ -673,85 +103,7 @@ public String[] ListDir(String tgt) {
 	 * Example usage: CloseFile(FH1)
 	 */
 	public FSReturnVals CloseFile(FileHandle ofh) {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		try{
-	
-			dos = new DataOutputStream(ClientSocket.getOutputStream());
-			dis = new DataInputStream(ClientSocket.getInputStream());
-			
-			}catch(Exception e){
-			
-		}
-
-		
-
-		String filePath = ofh.getFilePath();
-		
-		ArrayList<String> list = ofh.getChunkHandles();
-		ArrayList<Integer> offset = new ArrayList<Integer>();
-		String sizeOfList = Integer.toString(list.size()) + "-";
-		String chunkHandleList = "";
-		String offsetList = "";
-		for(int i = 0 ; i < list.size(); i ++){
-			offset.add(ofh.getLastOffset(list.get(i)));
-			chunkHandleList += (list.get(i) + "-");
-			offsetList += (offset.get(i) + "-");
-		}
-		String deleteFileCMD = "closeFile-";
-		byte[] CMD = deleteFileCMD.getBytes();
-		byte[] targetDirBytes = filePath.getBytes();
-		byte[] size = sizeOfList.getBytes();
-		byte[] chunkHandleListByte = chunkHandleList.getBytes();
-		byte[] offsetListByte = offsetList.getBytes();
-		
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try{
-		
-			outputStream.write(CMD);
-			outputStream.write(targetDirBytes);
-			outputStream.write(size);
-			outputStream.write(chunkHandleListByte);
-			outputStream.write(offsetListByte);
-			byte [] message = outputStream.toByteArray();
-			dos.write(message);
-			dos.flush();
-			
-			
-			int result = -1;
-			while(result == -1)
-			{
-				if(dis.available() > 0)
-				{
-					
-					byte [] b = new byte[4];
-					dis.read(b);
-					result = ByteBuffer.wrap(b).getInt();
-					
-					// 0 == SrcDirNotExistent
-					// 1 == DestDirExists
-					// 2 == success
-					switch(result){
-					case 0:
-						return FSReturnVals.BadHandle;
-					case 1:
-						return FSReturnVals.Success;
-													
-					}
-				}
-			}		
-				
-			
-		
-		}catch(Exception e){
-			
-		}
-		
 		return null;
-		
 	}
-	
-	
-
 
 }
--- CSCI485/src/com/client/ClientRec.java
+++ CSCI485/src/com/client/ClientRec.java
@@ -1,29 +1,11 @@
 package com.client;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 
-import com.chunkserver.ChunkServer;
 import com.client.ClientFS.FSReturnVals;
 
 public class ClientRec {
-	static int ServerPort = 3011;
-	static Socket ClientSocket;
-	static ObjectOutputStream WriteOutput;
-	static ObjectInputStream ReadInput;
-	private Client client = new Client();
-	public ClientRec(){
 
-			
-	}
 	/**
 	 * Appends a record to the open file as specified by ofh Returns BadHandle
 	 * if ofh is invalid Returns BadRecID if the specified RID is not null
@@ -33,52 +15,7 @@ public class ClientRec {
 	 * Example usage: AppendRecord(FH1, obama, RecID1)
 	 */
 	public FSReturnVals AppendRecord(FileHandle ofh, byte[] payload, RID RecordID) {
-		if(payload.length + 4 > ChunkServer.ChunkSize){
-			return FSReturnVals.RecordTooLong;
-		}
-		if(!RecordID.getChunkHandle().equals("")){
-			return FSReturnVals.BadRecID;
-		}
-		ArrayList<String> handles = ofh.getChunkHandles();
-		String newChunkHandle = "";
-		boolean writeSuccess = false;
-		if(handles.size() == 0){
-			//empty file so we should make new chunk
-			newChunkHandle = client.createChunk();
-			ofh.addChunkHandle(newChunkHandle);
-			ofh.setLastOffset(newChunkHandle, payload.length + 4);
-			writeSuccess = client.writeChunk(newChunkHandle, payload, 0);
-		}else{
-			newChunkHandle = handles.get(handles.size() - 1);
-			int lastOffset = ofh.getLastOffset(newChunkHandle);
-			// check it exceed 
-		
-			if((payload.length + lastOffset + 4) > ChunkServer.ChunkSize){
-				//exceed
-				newChunkHandle = client.createChunk();
-				ofh.addChunkHandle(newChunkHandle);
-				ofh.setLastOffset(newChunkHandle, payload.length);
-				writeSuccess = client.writeChunk(newChunkHandle, payload, 0);
-			}else{
-				
-				ofh.setLastOffset(newChunkHandle, lastOffset + payload.length + 4);
-				writeSuccess = client.writeChunk(newChunkHandle, payload, lastOffset);
-			}
-		}
-		
-		if(handles == null){
-			return FSReturnVals.BadHandle;
-		}
-		
-		
-	
-		if(!writeSuccess){
-			return null;
-		}else{
-			RecordID.setChunkHandle(newChunkHandle);
-			RecordID.setOffset(ofh.getLastOffset(newChunkHandle));
-			return FSReturnVals.Success;
-		}
+		return null;
 	}
 
 	/**
@@ -90,24 +27,7 @@ public class ClientRec {
 	 * Example usage: DeleteRecord(FH1, RecID1)
 	 */
 	public FSReturnVals DeleteRecord(FileHandle ofh, RID RecordID) {
-		ArrayList<String> handles = ofh.getChunkHandles();
-		if(handles.size() == 0){
-			return FSReturnVals.BadHandle;
-		}
-		int length = RecordID.getLength();
-		byte[] deleted = new byte[length];
-		for(int i = 0 ; i < length ; i ++){
-			deleted[i] = 33;
-		}
-			
-		
-		boolean result = client.writeChunk(RecordID.getChunkHandle(), deleted, RecordID.getOffset());
-	
-		if(result == true){
-			return FSReturnVals.Success;
-		}
 		return null;
-		
 	}
 
 	/**
@@ -117,24 +37,7 @@ public class ClientRec {
 	 * Example usage: ReadFirstRecord(FH1, tinyRec)
 	 */
 	public FSReturnVals ReadFirstRecord(FileHandle ofh, TinyRec rec){
-		ArrayList<String> handles = ofh.getChunkHandles();
-		if(handles.size() == 0){
-			return FSReturnVals.BadHandle;
-		}
-		String firstChunkHandle = handles.get(0);
-		byte[] result = client.readChunk(firstChunkHandle, 0, 0);
-		String resultString = new String(result, Charset.forName("UTF-8"));
-		if(resultString.equals("0")){
-			return FSReturnVals.RecDoesNotExist;
-		}
-		rec.setPayload(result);
-		RID rid = new RID();
-		rid.setChunkHandle(firstChunkHandle);
-		rid.setOffset(0);
-		rid.setLength(result.length);
-		rec.setRID(rid);
-		rec.setLength(result.length + 4);
-		return FSReturnVals.Success;
+		return null;
 	}
 
 	/**
@@ -156,49 +59,7 @@ public class ClientRec {
 	 * rec1, tinyRec2) 3. ReadNextRecord(FH1, rec2, tinyRec3)
 	 */
 	public FSReturnVals ReadNextRecord(FileHandle ofh, RID pivot, TinyRec rec){
-		String previousChunkHandle = pivot.getChunkHandle();
-		int previousOffset = pivot.getOffset();
-
-		ArrayList<String> handles = ofh.getChunkHandles();
-		String nextChunkHandle = "";
-		boolean nextChunkHandleExists = false;
-		for(int i = 0 ; i < handles.size();  i++){
-			if(handles.get(i).equals(previousChunkHandle)){
-				nextChunkHandle = handles.get(i +1);
-				nextChunkHandleExists =true;
-				break;
-			}
-		}
-		if(handles.size() == 0){
-			return FSReturnVals.BadHandle;
-		}
-		byte[] result = client.readChunk(previousChunkHandle, previousOffset, 1);
-		String resultString = new String(result, Charset.forName("UTF-8"));
-		byte[] newResult;
-		if(resultString.equals("0")){
-			return FSReturnVals.RecDoesNotExist;
-		}else if(resultString.equals("1")){
-			//get new chunk Handle 
-			if(nextChunkHandleExists){
-				newResult = client.readChunk(nextChunkHandle, 0, 0);
-				rec.setPayload(newResult);
-				RID rid = new RID();
-				rid.setChunkHandle(nextChunkHandle);
-				rid.setOffset(0);
-				rec.setRID(rid);
-				rid.setLength(newResult.length);
-				return FSReturnVals.Success;
-				
-			}
-			
-		}
-		rec.setPayload(result);
-		RID rid = new RID();
-		rid.setChunkHandle(previousChunkHandle);
-		rid.setOffset(previousOffset + 4 + pivot.getLength());
-		rid.setLength(result.length);
-		rec.setRID(rid);
-		return FSReturnVals.Success;
+		return null;
 	}
 
 	/**
